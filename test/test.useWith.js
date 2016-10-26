6e54303a731e493eef5ae35a0447afb2364863b3
Imam <imamudin.naseem@olacabs.com>
2016-10-26 22:47:39 +0530

    test(useWith): adding test for useWith

--- test/test.useWith.js
+++ test/test.useWith.js
@@ -4,13 +4,13 @@
 
 'use strict'
 
-import converge from '../utils/useWith'
+import useWith from '../utils/useWith'
 import test from 'ava'
 
 test('path', t => {
   const add = (a, b) => a + b
   const mult3 = a => a * 3
   const add3 = a => a * 3
-  t.deepEqual(converge(add, [add3, mult3])(3, 4), 21)
-  t.deepEqual(converge(add, [add3, mult3])(3, 0), 9)
+  t.deepEqual(useWith(add, [add3, mult3])(3, 4), 21)
+  t.deepEqual(useWith(add, [add3, mult3])(3, 0), 9)
 })
